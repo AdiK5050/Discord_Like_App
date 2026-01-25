6622385dbad1d3e57b6ec8a57d887f335c55db21
AdiK5050 <kumraditya456@gmail.com>
2026-01-25 23:58:29 +0530

    feat: optimised imports.

--- composeApp/src/commonMain/kotlin/io/adik5050/discord_like/ui/app/welcome/WelcomePage.kt
+++ composeApp/src/commonMain/kotlin/io/adik5050/discord_like/ui/app/welcome/WelcomePage.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.adik5050.discord_like.ui.app.login.composables.LoginBackground
-import io.adik5050.discord_like.ui.app.welcome.composables.WelcomeBackground
 import io.adik5050.discord_like.ui.app.welcome.composables.WelcomeButtons
 import io.adik5050.discord_like.ui.app.welcome.composables.WelcomeContent
 import io.adik5050.discord_like.ui.theme.AppTheme
