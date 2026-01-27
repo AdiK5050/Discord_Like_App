22152d916e5379ca9b6be01382908eb794b4e5db
AdiK5050 <kumraditya456@gmail.com>
2026-01-27 17:58:50 +0530

    chore: optimised imports.

--- composeApp/src/commonMain/kotlin/io/adik5050/discord_like/ui/app/navigation/main/MainNavigation.kt
+++ composeApp/src/commonMain/kotlin/io/adik5050/discord_like/ui/app/navigation/main/MainNavigation.kt
@@ -4,15 +4,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Surface
-import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.navigation3.runtime.NavBackStack
-import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
 import androidx.window.core.layout.WindowSizeClass
