1f37797b8634abe8266d0b045caaafb74fdc0e16
AdiK5050 <kumraditya456@gmail.com>
2026-01-27 17:32:00 +0530

    chore: optimised imports.

--- composeApp/src/commonMain/kotlin/io/adik5050/discord_like/ui/app/home_page/HomePage.kt
+++ composeApp/src/commonMain/kotlin/io/adik5050/discord_like/ui/app/home_page/HomePage.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
