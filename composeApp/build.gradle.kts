280f66cd8bcf60f7550bfd0d0c4ded53fa9e5a22
AdiK5050 <kumraditya456@gmail.com>
2026-02-10 19:19:44 +0530

    feat: added support and ksp dependencies for ios platforms.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -21,22 +21,25 @@ kotlin {
         }
     }
     
-//    listOf(
-//        iosArm64(),
-//        iosSimulatorArm64()
-//    ).forEach { iosTarget ->
-//        iosTarget.binaries.framework {
-//            baseName = "ComposeApp"
-//            isStatic = true
-//        }
-//    }
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+            //to select sql driver
+            linkerOpts.add("-lsqlite3")
+        }
+    }
     
     jvm()
     
-    js {
-        browser()
-        binaries.executable()
-    }
+//    js {
+//        browser()
+//        binaries.executable()
+//    }
 //
 //    @OptIn(ExperimentalWasmDsl::class)
 //    wasmJs {
@@ -125,6 +128,9 @@ dependencies {
     add("kspCommonMainMetadata", libs.androidx.room.compiler)
     add("kspAndroid", libs.androidx.room.compiler)
     add("kspJvm", libs.androidx.room.compiler)
+    add("kspIosX64", libs.androidx.room.compiler)
+    add("kspIosArm64", libs.androidx.room.compiler)
+    add("kspIosSimulatorArm64", libs.androidx.room.compiler)
 }
 room {
     schemaDirectory("$projectDir/schemas")
