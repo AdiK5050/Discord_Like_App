0600b0733efe3a47feed879d99cf04c8ec73b98d
AdiK5050 <kumraditya456@gmail.com>
2026-02-13 17:57:13 +0530

    chore: added required methods to support wannaverse image selector.

--- composeApp/src/androidMain/kotlin/io/adik5050/discord_like/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/io/adik5050/discord_like/MainActivity.kt
@@ -4,20 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fitInside
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.WindowInsetsRulers
+import com.wannaverse.imageselector.registerImageSelectorLauncher
+import com.wannaverse.imageselector.setImageSelectorActivity
 import io.adik5050.discord_like.storage.getDatabase
 import io.adik5050.discord_like.ui.app.App
-
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        setImageSelectorActivity(this)
+        registerImageSelectorLauncher()
         setContent {
             val appDatabase = getDatabase(this)
             App(
-                appDatabase
+                appDatabase = appDatabase,
             )
         }
     }
