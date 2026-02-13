35b18177bd5591dbd8f8b24f43f9505250cbaa73
AdiK5050 <kumraditya456@gmail.com>
2026-02-13 18:00:11 +0530

    chore: added ChatInfoViewModel and required parameters for viewmodel in ChatInfo composable.

--- composeApp/src/commonMain/kotlin/io/adik5050/discord_like/ui/app/chat_info/ChatInfo.kt
+++ composeApp/src/commonMain/kotlin/io/adik5050/discord_like/ui/app/chat_info/ChatInfo.kt
@@ -11,14 +11,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import io.adik5050.discord_like.storage.AppDatabase
 import io.adik5050.discord_like.ui.app.chat_info.composables.ChatInfoTopBar
 import io.adik5050.discord_like.ui.app.chat_info.composables.MediaInfo
 import io.adik5050.discord_like.ui.app.chat_info.composables.MembersInfo
+import io.adik5050.discord_like.ui.app.chat_info.viewmodels.ChatInfoViewModel
 import io.adik5050.discord_like.ui.theme.AppTheme
 
 @Composable
 fun ChatInfo(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    appDatabase: AppDatabase,
+    channelId: Int,
+    chatInfoViewModel: ChatInfoViewModel = viewModel { ChatInfoViewModel(appDatabase, channelId) }
 ) {
     Surface (
         modifier = modifier
@@ -42,21 +48,3 @@ fun ChatInfo(
     }
 }
 
-@Preview
-@Composable
-fun PreviewChatInfo() {
-    AppTheme {
-        Surface {
-            ChatInfo()
-        }
-    }
-}
-@Preview
-@Composable
-fun PreviewChatInfoDark() {
-    AppTheme (darkTheme = true){
-        Surface {
-            ChatInfo()
-        }
-    }
-}
---
+++ composeApp/src/commonMain/kotlin/io/adik5050/discord_like/ui/app/chat_info/viewmodels/ChatInfoViewModel.kt
@@ -0,0 +1,11 @@
+package io.adik5050.discord_like.ui.app.chat_info.viewmodels
+
+import androidx.lifecycle.ViewModel
+import io.adik5050.discord_like.storage.AppDatabase
+
+class ChatInfoViewModel(
+    appDatabase: AppDatabase,
+    channelId: Int
+): ViewModel(){
+
+}
