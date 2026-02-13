2b6546ba04eddf16ab5ebe23f2853096888d57fd
AdiK5050 <kumraditya456@gmail.com>
2026-02-13 18:01:24 +0530

    chore: optimised imports.

--- composeApp/src/commonMain/kotlin/io/adik5050/discord_like/ui/app/chat_info/ChatInfo.kt
+++ composeApp/src/commonMain/kotlin/io/adik5050/discord_like/ui/app/chat_info/ChatInfo.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.adik5050.discord_like.storage.AppDatabase
@@ -17,7 +16,6 @@ import io.adik5050.discord_like.ui.app.chat_info.composables.ChatInfoTopBar
 import io.adik5050.discord_like.ui.app.chat_info.composables.MediaInfo
 import io.adik5050.discord_like.ui.app.chat_info.composables.MembersInfo
 import io.adik5050.discord_like.ui.app.chat_info.viewmodels.ChatInfoViewModel
-import io.adik5050.discord_like.ui.theme.AppTheme
 
 @Composable
 fun ChatInfo(
