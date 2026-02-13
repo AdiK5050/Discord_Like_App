249ee5cdf5e61459fdd66ec6a7d63cae1e5287d3
AdiK5050 <kumraditya456@gmail.com>
2026-02-13 17:44:41 +0530

    chore: optimised imports.

--- composeApp/src/jvmMain/kotlin/io/adik5050/discord_like/main.kt
+++ composeApp/src/jvmMain/kotlin/io/adik5050/discord_like/main.kt
@@ -1,8 +1,5 @@
 package io.adik5050.discord_like
 
-import androidx.compose.foundation.layout.fitInside
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.WindowInsetsRulers
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import io.adik5050.discord_like.storage.getDatabase
