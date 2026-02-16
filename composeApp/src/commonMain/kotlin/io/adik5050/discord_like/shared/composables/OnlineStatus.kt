55c053a162e62ab1b9078669fa2ada2ad5d3779c
AdiK5050 <kumraditya456@gmail.com>
2026-02-16 16:37:21 +0530

    chore: updated usages of new columns of UserEntity in ChatViewModel and MessageContent. -> added a type converter from string to OnlineStatus.

--- composeApp/src/commonMain/kotlin/io/adik5050/discord_like/shared/composables/OnlineStatus.kt
+++ composeApp/src/commonMain/kotlin/io/adik5050/discord_like/shared/composables/OnlineStatus.kt
@@ -1,6 +1,7 @@
 package io.adik5050.discord_like.shared.composables
 
 import androidx.compose.ui.graphics.Color
+import io.adik5050.discord_like.shared.composables.OnlineStatus.ONLINE
 import myapplication.composeapp.generated.resources.Res
 import myapplication.composeapp.generated.resources.edit
 import myapplication.composeapp.generated.resources.status_do_not_disturb
@@ -15,4 +16,11 @@ enum class OnlineStatus(val icon: DrawableResource, val description: StringResou
     DO_NOT_DISTURB(Res.drawable.status_do_not_disturb, Res.string.status_offline, Color(0xfff02d2d)),
     OFFLINE(Res.drawable.status_offline, Res.string.status_offline, Color.Gray),
     EDIT(Res.drawable.edit, Res.string.status_edit, Color.Gray)
+}
+fun convertToOnlineStatus(onlineStatus: String?) : OnlineStatus {
+    if(onlineStatus.equals(null)) return OnlineStatus.OFFLINE
+    OnlineStatus.entries.forEach {
+        if(onlineStatus!!.equals(it.description)) return it
+    }
+    return ONLINE
 }
--- composeApp/src/commonMain/kotlin/io/adik5050/discord_like/ui/app/chat/composables/MessageContent.kt
+++ composeApp/src/commonMain/kotlin/io/adik5050/discord_like/ui/app/chat/composables/MessageContent.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.adik5050.discord_like.shared.composables.OnlineStatus
+import io.adik5050.discord_like.shared.composables.convertToOnlineStatus
 import io.adik5050.discord_like.storage.MessageEntity
 import io.adik5050.discord_like.ui.app.chat.viewmodels.UserInfo
 
@@ -27,8 +28,8 @@ fun MessageContent (
                 val user = channelMembers.firstOrNull { it.userId == message.senderId }
                 MessageCard(
                     image = user?.profileImage,
-                    status = OnlineStatus.OFFLINE,
-                    name = user?.username,
+                    status = convertToOnlineStatus(user?.onlineStatus),
+                    name = user?.displayName,
                     time = message.sentAt,
                     message = message.message.decodeToString(),
                 )
@@ -36,132 +37,3 @@ fun MessageContent (
         }
     }
 }
-val messageCardDataListData = listOf<MessageCardData>(
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.DO_NOT_DISTURB,
-        name = "Rebecca",
-        time = "7:43",
-        message = "What Gives, I'll have it set up soon!"
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.DO_NOT_DISTURB,
-        name = "Rebecca",
-        time = "7:43",
-        message = "What Gives, I'll have it set up soon!"
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.DO_NOT_DISTURB,
-        name = "Rebecca",
-        time = "7:43",
-        message = "What Gives, I'll have it set up soon!"
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.DO_NOT_DISTURB,
-        name = "Rebecca",
-        time = "7:43",
-        message = "What Gives, I'll have it set up soon!"
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.DO_NOT_DISTURB,
-        name = "Rebecca",
-        time = "7:43",
-        message = "What Gives, I'll have it set up soon!"
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.DO_NOT_DISTURB,
-        name = "Rebecca",
-        time = "7:43",
-        message = "What Gives, I'll have it set up soon!"
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.OFFLINE,
-        name = "Roberto",
-        time = "4:38",
-        message = "Guess what? The slacker thinks he is in control."
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.OFFLINE,
-        name = "Roberto",
-        time = "4:38",
-        message = "Guess what? The slacker thinks he is in control."
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.OFFLINE,
-        name = "Roberto",
-        time = "4:38",
-        message = "Guess what? The slacker thinks he is in control."
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.OFFLINE,
-        name = "Roberto",
-        time = "4:38",
-        message = "Guess what? The slacker thinks he is in control."
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.OFFLINE,
-        name = "Roberto",
-        time = "4:38",
-        message = "Guess what? The slacker thinks he is in control."
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.OFFLINE,
-        name = "Roberto",
-        time = "4:38",
-        message = "Guess what? The slacker thinks he is in control."
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.ONLINE,
-        name = "Richard",
-        time = "6:25",
-        message = "I don't have any sample data and it sucks!"
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.ONLINE,
-        name = "Richard",
-        time = "6:25",
-        message = "I don't have any sample data and it sucks!"
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.ONLINE,
-        name = "Richard",
-        time = "6:25",
-        message = "I don't have any sample data and it sucks!"
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.ONLINE,
-        name = "Richard",
-        time = "6:25",
-        message = "I don't have any sample data and it sucks!"
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.ONLINE,
-        name = "Richard",
-        time = "6:25",
-        message = "I don't have any sample data and it sucks!"
-    ),
-    MessageCardData(
-        image = null,
-        status = OnlineStatus.ONLINE,
-        name = "Richard",
-        time = "6:25",
-        message = "I don't have any sample data and it sucks!"
-    ),
-
-)
