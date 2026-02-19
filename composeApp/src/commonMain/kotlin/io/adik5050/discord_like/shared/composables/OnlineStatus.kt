4950e519961137f09f90c888a5f0ec2c1ef30f6b
AdiK5050 <kumraditya456@gmail.com>
2026-02-19 13:57:42 +0530

    chore: fixed string resource.

--- composeApp/src/commonMain/kotlin/io/adik5050/discord_like/shared/composables/OnlineStatus.kt
+++ composeApp/src/commonMain/kotlin/io/adik5050/discord_like/shared/composables/OnlineStatus.kt
@@ -13,7 +13,7 @@ import org.jetbrains.compose.resources.StringResource
 
 enum class OnlineStatus(val icon: DrawableResource, val description: StringResource, val color: Color) {
     ONLINE(Res.drawable.status_online, Res.string.status_online, Color(0xff00c230)),
-    DO_NOT_DISTURB(Res.drawable.status_do_not_disturb, Res.string.status_offline, Color(0xfff02d2d)),
+    DO_NOT_DISTURB(Res.drawable.status_do_not_disturb, Res.string.status_do_not_disturb, Color(0xfff02d2d)),
     OFFLINE(Res.drawable.status_offline, Res.string.status_offline, Color.Gray),
     EDIT(Res.drawable.edit, Res.string.status_edit, Color.Gray)
 }
