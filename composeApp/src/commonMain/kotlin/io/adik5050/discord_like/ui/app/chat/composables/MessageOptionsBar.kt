cf0c2f0c56cf951bbcb10507eb30f0678da825c3
AdiK5050 <kumraditya456@gmail.com>
2026-03-09 19:08:43 +0530

    chore: added new Composables to show options for messages.

---
+++ composeApp/src/commonMain/kotlin/io/adik5050/discord_like/ui/app/chat/composables/MessageOptionsBar.kt
@@ -0,0 +1,43 @@
+package io.adik5050.discord_like.ui.app.chat.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun MessageOptionsBar(
+    modifier: Modifier = Modifier,
+    onClickOption: (Int) -> Unit
+) {
+    Card(
+        modifier = modifier
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            basicMessageOptions.forEach {
+                IconButton(
+                    onClick = {
+                        onClickOption(it.optionId)
+                    }
+                ) {
+                    Icon(
+                        modifier = Modifier.size(16.dp),
+                        painter = painterResource(it.icon),
+                        contentDescription = stringResource(it.description)
+                    )
+                }
+            }
+        }
+    }
+}
